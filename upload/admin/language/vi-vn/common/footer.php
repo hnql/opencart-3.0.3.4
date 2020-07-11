6579ad53782992c276e60bc04894ae2cb4a4e31d
Quang <hnql1991@gmail.com>
2020-07-11 09:50:07 +0700

    Add Vietnamese

---
+++ upload/admin/language/vi-vn/common/footer.php
@@ -0,0 +1,4 @@
+<?php
+// Text
+$_['text_footer']  = '<a href="http://www.a-cart.online">Phiên bản tiếng việt thuộc bản quyền của a-cart.online</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
+$_['text_version'] = 'Phiên bản %s';
